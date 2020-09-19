4e9395cb2be645d33d4039f2e0fd6f0d01ce50d1
FarhadGSRX <farhad.ghamsari@gmail.com>
2020-09-19 13:42:17 -0500

    Correcting the way canvases swap.

--- dashpi.py
+++ dashpi.py
@@ -11,18 +11,9 @@ options = RGBMatrixOptions()
 options.rows = 32
 options.cols = 32
 options.chain_length = 4
-options.brightness = 30
+options.brightness = 20
 options.gpio_slowdown = 4
 options.hardware_mapping = "regular"
-
-# Matrix Vars
-dayBright = 50
-nightBright = 15
-
-mx = RGBMatrix(options=options)
-canvas1 = mx.CreateFrameCanvas()
-canvas2 = mx.CreateFrameCanvas()
-canvi = [canvas1, canvas2]
 
 textColor = graphics.Color(255, 0, 255)
 font = graphics.Font()
@@ -32,8 +23,6 @@ fontHuge.LoadFont("/home/dietpi/DashPi/Misc/fonts/20x40.bdf")
 
 # Old Notes I'm saving in case I run into old problems
 # os.environ['http_proxy']='' #Per http://code.activestate.com/lists/python-list/617037/ fixes an Errno -2 error I was getting during updateWU ##10/18/16 Commented this line out due to addition of the internetchecker. Can uncomment if problems suddenly reappear.
-
-graphics.DrawText(canvi[0], font, 5, 29, graphics.Color(255, 0, 255), time.strftime("%H:%M:%S"))
 
 def isNight():
     return int(time.strftime("%H")) < 6
@@ -45,6 +34,7 @@ def canvasFlip():
 
 def regCycle():
     logging.info('Main - Reg Cycle')
+    #offset_canvas = mx.SwapOnVSync(offset_canvas)
     mx.SwapOnVSync(canvi[0])
     graphics.DrawText(canvi[1], font, 15, 29, graphics.Color(255, 0, 255), time.strftime("%H:%M:%S"))
     canvasFlip()
@@ -64,14 +54,24 @@ def regCycle():
 
 
 def go(currentlyLogging=False):
+    mx = RGBMatrix(options=options)
+    offset_canvas = mx.CreateFrameCanvas()
+    pos = 10
+
     while True:
-        regCycle()
-        time.sleep(0.5)
+        offset_canvas.Clear()
+        len = graphics.DrawText(offset_canvas, font, pos, 10, textColor, time.strftime("%H:%M:%S"))
+        pos -= 2
+        if (pos + len < 0):
+            pos = 60
+        offset_canvas = mx.SwapOnVSync(offset_canvas)
+        time.sleep(0.1)
+
 
 
 # Main function
 if __name__ == "__main__":
-    #logging.basicConfig(level=logging.DEBUG, filename="DashPi_logfile.txt", filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
-    #logging.info('DashPi Main Called In, Starting up')
+    logging.basicConfig(level=logging.DEBUG, filename="DashPi_logfile.txt", filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
+    logging.info('DashPi Main Called In, Starting up')
     go()
-    #logging.info('Exiting for some reason?')
+    logging.info('Exiting for some reason?')
