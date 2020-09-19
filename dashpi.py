629e3bcafb118e474dc17529e39329da7e421076
FarhadGSRX <farhad.ghamsari@gmail.com>
2020-09-19 14:59:03 -0500

    Major Update, all day clock, as well as first day and date update.

--- dashpi.py
+++ dashpi.py
@@ -11,7 +11,7 @@ options = RGBMatrixOptions()
 options.rows = 32
 options.cols = 32
 options.chain_length = 4
-options.brightness = 20
+options.brightness = 50
 options.gpio_slowdown = 4
 options.hardware_mapping = "regular"
 
@@ -20,6 +20,43 @@ font = graphics.Font()
 fontHuge = graphics.Font()
 font.LoadFont("/home/dietpi/DashPi/Misc/fonts/10x20.bdf")
 fontHuge.LoadFont("/home/dietpi/DashPi/Misc/fonts/20x40.bdf")
+
+# Daytime Color Spectrum
+daytime_color_spectrum = {0: graphics.Color(21, 40, 108),
+                          130: graphics.Color(21, 41, 114),
+                          300: graphics.Color(21, 41, 114),
+                          430: graphics.Color(118, 80, 101),
+                          600: graphics.Color(253, 193, 17),
+                          730: graphics.Color(255, 241, 68),
+                          900: graphics.Color(255, 255, 255),
+                          1030: graphics.Color(166, 220, 230),
+                          1200: graphics.Color(69, 196, 215),
+                          1330: graphics.Color(225, 243, 245),
+                          1500: graphics.Color(255, 255, 255),
+                          1630: graphics.Color(255, 249, 203),
+                          1800: graphics.Color(251, 240, 0),
+                          1930: graphics.Color(253, 184, 21),
+                          2100: graphics.Color(247, 148, 29),
+                          2230: graphics.Color(74, 61, 107)}
+
+def color_the_time():
+    # initializing nearest key
+    search_key = int(time.strftime("%H%M"))
+
+
+    # Using list comprehension + keys() + lambda
+    # Closest key in dictionary
+    res = daytime_color_spectrum.get(search_key) or daytime_color_spectrum[min(daytime_color_spectrum.keys(), key=lambda key: abs(key - search_key))]
+    # the min() function takes an argument "key" which defines "how to define the minimum" of the given list.
+    # In this case, looping through every value of the list as variable "key" and subtracting the search_key, taking
+    # the value closest to 0
+    # This line is wizardry, to understand it better, read these:
+    # https://www.geeksforgeeks.org/python-find-closest-number-to-k-in-given-list/?ref=lbp
+    # https://www.geeksforgeeks.org/python-find-the-closest-key-in-dictionary/
+
+    return res
+
+
 
 # Old Notes I'm saving in case I run into old problems
 # os.environ['http_proxy']='' #Per http://code.activestate.com/lists/python-list/617037/ fixes an Errno -2 error I was getting during updateWU ##10/18/16 Commented this line out due to addition of the internetchecker. Can uncomment if problems suddenly reappear.
@@ -36,7 +73,7 @@ def regCycle():
     logging.info('Main - Reg Cycle')
     #offset_canvas = mx.SwapOnVSync(offset_canvas)
     mx.SwapOnVSync(canvi[0])
-    graphics.DrawText(canvi[1], font, 15, 29, graphics.Color(255, 0, 255), time.strftime("%H:%M:%S"))
+    graphics.DrawText(canvi[1], font, 15, 29, color_the_time(), time.strftime("%H:%M:%S"))
     canvasFlip()
 
 
@@ -58,14 +95,22 @@ def go(currentlyLogging=False):
     offset_canvas = mx.CreateFrameCanvas()
     pos = 10
 
+    """ This block moves horizontally
     while True:
         offset_canvas.Clear()
-        len = graphics.DrawText(offset_canvas, font, pos, 10, textColor, time.strftime("%H:%M:%S"))
+        len = graphics.DrawText(offset_canvas, font, pos, 30, textColor, time.strftime("%H:%M:%S"))
         pos -= 2
         if (pos + len < 0):
             pos = 60
         offset_canvas = mx.SwapOnVSync(offset_canvas)
         time.sleep(0.1)
+    """
+
+    while True:
+        offset_canvas.Clear()
+        len = graphics.DrawText(offset_canvas, fontHuge, 3, 30, textColor, time.strftime("%H:%M"))
+        graphics.DrawText(offset_canvas, font, len + 5, 15, textColor, time.strftime("%a"))
+        graphics.DrawText(offset_canvas, font, len + 5, 30, textColor, time.strftime("%m/%d"))
 
 
 
